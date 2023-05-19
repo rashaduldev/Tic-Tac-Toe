a369c771074075ade393c38298e70f53bfc7c8e8
Rashadul Islam <rashadul.dev@gmail.com>
2023-05-19 17:52:03 +0600

    Update index.c

--- index.c
+++ index.c
@@ -4,19 +4,37 @@
 void printBoard();
 char board[]={'0','1','2','3','4','5','6','7','8','9'};
 void main(){
+    int player=1;
+    char mark=(player=1) ? 
    printBoard();
 }
 void printBoard(){
     printf("\n\n");
     printf("*** tik tac toe *** \n");
     printf("    |     |     \n");
-    printf(" %c  |  %c  |  %c  \n",board);
+    printf(" %c  |  %c  |  %c  \n",board[1], board[2],board[3]);
     printf("____|_____|___  \n");
     printf("    |     |     \n");
-    printf(" %c  |  %c  |  %c  \n");
+    printf(" %c  |  %c  |  %c  \n",board[4], board[5],board[6]);
     printf("____|_____|___  \n");
     printf("    |     |     \n");
-    printf(" %c  |  %c  |  %c  \n");
+    printf(" %c  |  %c  |  %c  \n",board[7], board[8],board[9]);
     printf("    |     |     \n");
      printf("\n\n");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
